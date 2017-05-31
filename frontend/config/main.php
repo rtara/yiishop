1726504820ebf05147d4a9afc6913e600f16040f
web6 <web6@it-step.edu>
2017-05-31 18:20:33 +0300

    Language

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,12 +8,15 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+	'language'=>'uk-UK',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+	
         'request' => [
             'csrfParam' => '_csrf-frontend',
+			'class' => 'frontend\components\LangRequest'
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -37,15 +40,29 @@ return [
             'errorAction' => 'site/error',
         ],
         
-        'urlManager' => [
+        /*'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+			'class'=>'frontend\components\LangUrlManager',
             'rules' => [
-				'posts' => 'post/index', 
-				'post/<id:\d+>' => 'post/view',
+				'/' => 'site/index', 
+				//'<controller:\w+>/<action:\w+>/*'=>'<controller>/<action>',
             ],
-        ],
+        ],*/
         
+		'i18n' => [
+			'translations' => [
+				'*' => [
+					'class' => 'yii\i18n\PhpMessageSource',
+					'basePath' => '@frontend/messages',
+					'sourceLanguage' => 'uk',
+					'fileMap' => [
+						//'main' => 'main.php',
+					],
+				],
+			],
+		],
+		
         'view' => [
             'theme' => [
                 'pathMap' => [
