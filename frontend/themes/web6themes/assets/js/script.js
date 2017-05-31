c6fd4ec396d7049583772ddd0290811f6c5df25a
Alex <aizsargs@gmail.com>
2017-05-31 08:39:55 -0700

    finished index design

---
+++ frontend/themes/web6themes/assets/js/script.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date()).getTime() + 11*24*60*60*1000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+	$('#horizontalTab').easyResponsiveTabs({
+            type: 'default', //Types: default, vertical, accordion
+            width: 'auto', //auto or any width like 600px
+            fit: true   // 100% fit in a container
+        });
+
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+
+    $("#flexiselDemo1").flexisel({
+        visibleItems: 4,
+        animationSpeed: 1000,
+        autoPlay: true,
+        autoPlaySpeed: 3000,
+        pauseOnHover: true,
+        enableResponsiveBreakpoints: true,
+        responsiveBreakpoints: {
+            portrait: {
+                changePoint:480,
+                visibleItems: 1
+            },
+            landscape: {
+                changePoint:640,
+                visibleItems:2
+            },
+            tablet: {
+                changePoint:768,
+                visibleItems: 3
+            }
+        }
+    });
+
+	 
+
+
+});
