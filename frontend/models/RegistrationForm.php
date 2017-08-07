145ab1c8c7c1a5f341c466b87ff7c7ffb4728d18
Alex Ustychenko <Alex Ustychenko>
2017-08-07 09:00:55 -0700

    fix registratin problem

---
+++ frontend/models/RegistrationForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace frontend\models;
+
+use dektrium\user\models\Profile;
+use dektrium\user\models\RegistrationForm as BaseRegistrationForm;
+use dektrium\user\models\User;
+use yii\base\Model;
+
+
+class RegistrationForm extends BaseRegistrationForm
+{
+    /**
+     * Add a new field
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+        $rules[] = ['name', 'required'];
+        $rules[] = ['name', 'string', 'max' => 255];
+        return $rules;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        $labels = parent::attributeLabels();
+        $labels['name'] = \Yii::t('user', 'Name');
+        return $labels;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function loadAttributes(User $user)
+    {
+        // here is the magic happens
+        $user->setAttributes([
+            'email'    => $this->email,
+            'username' => $this->username,
+            'password' => $this->password,
+        ]);
+        /** @var Profile $profile */
+        $profile = \Yii::createObject(Profile::className());
+        $profile->setAttributes([
+            'name' => $this->name,
+        ]);
+        $user->setProfile($profile);
+    }
+}
